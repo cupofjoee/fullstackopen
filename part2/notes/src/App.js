bcbde602053134be3e56b708305b227d3b11dff2
Joe <jteddysanjaya@gmail.com>
2020-08-14 21:10:41 +0800

    notes practice

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification"
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const hook = () => {
     noteService.getAll().then((notes) => {
@@ -38,8 +40,17 @@ const App = () => {
     const note = notes.find((note) => note.id == id);
     const changedNote = { ...note, important: !note.important };
 
-    noteService.update(id, changedNote).then((returnedNote) => {
+    noteService
+      .update(id, changedNote).then((returnedNote) => {
       setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+    }).catch(error => {
+      setErrorMessage(
+        `Note ${note.content} was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      setNotes(notes.filter(n => n.id !== id))
     });
   };
 
@@ -48,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <ul>
         {notesToShow.map((note) => (
           <Note
