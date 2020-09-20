80705e3b280c9b757675dc792d0984305bb34d7e
Joe <jteddysanjaya@gmail.com>
2020-09-21 00:56:04 +0800

    build

--- part3/the-phonebook/index.js
+++ part3/the-phonebook/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
@@ -90,7 +94,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
