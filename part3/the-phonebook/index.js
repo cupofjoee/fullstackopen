f8bbe61369c1a4c4720975cf2b301d30493aaa84
Joe <jteddysanjaya@gmail.com>
2020-09-21 01:31:56 +0800

    porting

--- part3/the-phonebook/index.js
+++ part3/the-phonebook/index.js
@@ -94,7 +94,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT =  process.env.PORT
+const PORT =  process.env.PORT || 50602
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
