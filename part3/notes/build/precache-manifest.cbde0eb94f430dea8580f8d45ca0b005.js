c98436199cd50f7e3bc7288ad8d782652b4fc818
Joe <jteddysanjaya@gmail.com>
2020-09-21 00:35:48 +0800

    notes practice

---
+++ part3/notes/build/precache-manifest.cbde0eb94f430dea8580f8d45ca0b005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c016860f101cad14674a21222539553c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99fe7cffa534457ed8dd",
+    "url": "/static/css/main.e8765034.chunk.css"
+  },
+  {
+    "revision": "2bca4a2f2c8e576ef16a",
+    "url": "/static/js/2.e38ffbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e38ffbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fe7cffa534457ed8dd",
+    "url": "/static/js/main.50059056.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
