aafd3be2a72788ae43ec19025ab1537b396fa5ed
Joe <jteddysanjaya@gmail.com>
2020-09-26 09:34:42 +0800

    MongoDB

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(cors())
@@ -32,34 +34,20 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    // If note is undefined return status 404
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id != id)
-
-    response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
   
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -70,19 +58,19 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
