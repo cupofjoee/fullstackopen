f74298fb3786ce4294c29a348cbb42d1a38c97e2
Joe <jteddysanjaya@gmail.com>
2020-10-02 15:32:46 +0800

    chapter 4c

--- part3/notes/controllers/notes.js
+++ part3/notes/controllers/notes.js
@@ -1,13 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({}).populate('user', {
+    username: 1,
+    name: 1
   })
+  response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
+notesRouter.get('/:id', async (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -19,31 +22,31 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async  (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     date: body.date,
-    important: body.important || false
+    important: body.important || false,
+    user: user._id
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -51,11 +54,8 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important
   })
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
--- part3/notes/utils/config.js
+++ part3/notes/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config({ path: '../.env' })
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV == 'test') {
+  MONGODB_URI = process.env.MONGODB_URI_TEST
+}
 
 module.exports = {
   MONGODB_URI,
